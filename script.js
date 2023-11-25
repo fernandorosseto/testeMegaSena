3722a82e6e527b8b275b1030099e7ac6e9124b84
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2023-11-25 14:32:09 -0300

    ajuste no js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRYOEs4QM-0kKjx1NPpj9LfAHWcNGSikmC5EKSBs9NUkZCG8usYOcz5bjelCJRhEUBtvFlSAr8nRDv5/pub?gid=0&single=true&output=csv";
 
-function planilhaDb() {
-  const response = fetch(url)
-    .then((response) => response.text())
-    .then((data) => console.log(Papa.parse(data, { header: true })))
-    .catch((error) => console.log(error));
+// function planilhaDb() {
+//   const response = fetch(url)
+//     .then((response) => response.text())
+//     .then((data) => console.log(Papa.parse(data, { header: true })))
+//     .catch((error) => console.log(error));
+// }
+
+// planilhaDb();
+
+async function planilhaDb() {
+  const response = await fetch(url);
+  const data = await response.text();
+  const planilhaData = Papa.parse(data, { header: true }).data;
+
+  // Imprime todos os dados da planilha
+  console.log("Planilha da Mega Sena:", planilhaData);
+
+  // Acessa o último item da array
+  const ultimoItem = planilhaData[planilhaData.length - 1];
+
+  // Imprime o último item da planilha
+  console.log("Último item da Planilha da Mega Sena:", ultimoItem);
 }
 
 planilhaDb();
+
 
 let dezenasMegaSena;
 
@@ -19,8 +37,8 @@ async function buscarUltimoResultado() {
 
     if (resposta.status === 200) {
       const dados = await resposta.json();
-      dezenasMegaSena = dados.listaDezenas;
-      return { concurso: dados.listaDezenas };
+      dezenasMegaSena = [dados.numero, ...dados.listaDezenas];
+      return { concurso: dezenasMegaSena };
     } else {
       console.error("Falha ao buscar resultados da Mega Sena.");
       return null;
@@ -41,21 +59,23 @@ async function obterUltimoResultado() {
 async function ultimoResultado() {
   const dezenas = await obterUltimoResultado();
   console.log("Último Resultado da Mega Sena:", dezenas);
-  const enviarDados = () => {
-  fetch('https://api.sheetmonkey.io/form/iB9B4iWoGiZiRCrZxFVfkq', {
-    method: 'post',
-    headers: {
-      'accept': 'application/json',
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(dezenas)
-  })
+
+  if (ultimaArrayPlanilha && dezenas[0] === ultimaArrayPlanilha.numero){
+    console.log("Jogo já incluído na planilha. Não enviar dados.");
+  } else {
+    // Adicionar o último resultado à planilha
+    const enviarDados = () => {
+    fetch('https://api.sheetmonkey.io/form/iB9B4iWoGiZiRCrZxFVfkq', {
+      method: 'post',
+      headers: {
+        'accept': 'application/json',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(dezenas)
+    })
+    }
+
+    enviarDados();
   }
-
-  enviarDados();
-
 }
-
 ultimoResultado();
-
-
